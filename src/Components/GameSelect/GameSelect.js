431e51a0328f98256deb1924543df69c2b6923b0
Logan Weekes <ltweekes1@gmail.com>
2022-09-13 00:16:00 -0400

    add profile pic size adjustment

--- src/Components/GameSelect/GameSelect.js
+++ src/Components/GameSelect/GameSelect.js
@@ -1,9 +1,13 @@
 import './GameSelect.scss';
+import { Player } from '../Player/Player';
 
 export const GameSelect = (props) => {
     return (
         <div className='account'>
-            <h1>Pocket Cards</h1>
+            <header>
+                <Player theme='red' size='small'/>
+                <h1>Pocket Cards</h1>
+            </header>
         </div>
     )
 }
--- src/Components/Player/Player.js
+++ src/Components/Player/Player.js
@@ -5,12 +5,13 @@ import PropTypes from 'prop-types';
 export const Player = (props) => {
     return (
         <div className='player'>
-            <img src={girl2} className={props.theme}/>
-            <p className={props.theme}>logan3124</p>
+            <img src={girl2} className={`${props.theme} + ${props.size}`}/>
+            <p className={`${props.theme} + ${props.size}`}>logan3124</p>
         </div>
     )
 }
 
 Player.propTypes = {
+    size: PropTypes.string,
     theme: PropTypes.string.isRequired
 }
